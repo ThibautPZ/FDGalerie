78905c635b54332b4e70e9168caba0253e7e4ed5
Thibaut Plez <thibautplez@gmail.com>
2025-07-05 02:09:36 +0200

    [Frontend] Modified FoldableComponent component to adapt to ArrowSelector changes

--- frontend/src/components/FoldableComponent.jsx
+++ frontend/src/components/FoldableComponent.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 import ArrowSelector from "./customComponents/ArrowSelector";
 
-function FoldableComponent({
+export default function FoldableComponent({
   className,
-  labelNS,
+  labelText,
   isOpen,
   whenLabel,
   onOpen,
@@ -15,7 +15,7 @@ function FoldableComponent({
   return (
     <div className={className || ""}>
       <ArrowSelector
-        labelNS={labelNS}
+        labelText={labelText}
         whenLabel={whenLabel}
         isFolded={isFolded}
         setIsFolded={setIsFolded}
@@ -26,16 +26,3 @@ function FoldableComponent({
     </div>
   );
 }
-
-// FoldableComponent.propTypes = {
-//   classname: PropTypes.string,
-//   labelNS: PropTypes.string,
-//   isOpen: PropTypes.bool,
-//   whenLabel: PropTypes.string,
-//   children: PropTypes.,
-// };
-
-// FoldableComponent.defaultProps = {
-//   watch: null,
-// };
-export default FoldableComponent;
