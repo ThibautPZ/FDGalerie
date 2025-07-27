c14b50049a547a8e559b37be37a8dac35d5e1362
ThibautPZ <thibautplez@gmail.com>
2025-07-27 19:38:36 +0200

    [BACKEND] Addition : stringFunctions : caseToUppercase to convert a string to UPPERCASE case

--- backend/src/services/stringFunctions.js
+++ backend/src/services/stringFunctions.js
@@ -1,4 +1,9 @@
 const { isStringNotEmpty } = require("./typesAndValidationChecks");
+const {
+  characterNotLowerCharRegExp,
+  punctuationRegExp,
+  digitRegExp,
+} = require("./regularExpressions");
 
 const uppercaseFirstChar = (str) => {
   if (!isStringNotEmpty(str)) {
@@ -9,4 +14,57 @@ const uppercaseFirstChar = (str) => {
   return `${uppercasedChar}${remainingChars}`;
 };
 
-module.exports = { uppercaseFirstChar };
+const caseToUpperCase = (str) => {
+  if (!isStringNotEmpty(str)) {
+    return "";
+  }
+  const trimmedStr = str.trim();
+  let returnedStr = "";
+  let prevMatchIndex = -1;
+  let prevMatchType = "";
+
+  for (let i = 0; i < trimmedStr.length; i += 1) {
+    const char = trimmedStr[i];
+    if (punctuationRegExp.test(char)) {
+      if (
+        i !== 0 &&
+        (prevMatchIndex !== i - 1 ||
+          (prevMatchType !== "punctuation" && prevMatchIndex === i - 1))
+      ) {
+        returnedStr = `${returnedStr}_`;
+      }
+      prevMatchIndex = i;
+      prevMatchType = "punctuation";
+    } else if (digitRegExp.test(char)) {
+      if (
+        i !== 0 &&
+        (prevMatchIndex !== i - 1 ||
+          (prevMatchType !== "number" && prevMatchIndex === i - 1))
+      ) {
+        returnedStr = `${returnedStr}_${char}`;
+      } else {
+        returnedStr = `${returnedStr}${char}`;
+      }
+      prevMatchIndex = i;
+      prevMatchType = "number";
+    } else if (characterNotLowerCharRegExp.test(char)) {
+      if (
+        i !== 0 &&
+        (prevMatchIndex !== i - 1 ||
+          (prevMatchType !== "other" && prevMatchIndex === i - 1))
+      ) {
+        returnedStr = `${returnedStr}_${char}`;
+      } else {
+        returnedStr = `${returnedStr}${char}`;
+      }
+      prevMatchIndex = i;
+      prevMatchType = "other";
+    } else {
+      returnedStr = `${returnedStr}${char}`;
+    }
+  }
+
+  return returnedStr.toLocaleUpperCase();
+};
+
+module.exports = { uppercaseFirstChar, caseToUpperCase };
