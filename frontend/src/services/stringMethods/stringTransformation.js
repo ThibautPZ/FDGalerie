bc07aed84821eec13500b6fd6b23cf2d676519fd
ThibautPZ <thibautplez@gmail.com>
2025-07-27 19:44:10 +0200

    [FRONTEND] Addition : stringTransformations : caseToUppercase to convert a string to UPPERCASE case

--- frontend/src/services/stringMethods/stringTransformation.js
+++ frontend/src/services/stringMethods/stringTransformation.js
@@ -1,3 +1,10 @@
+import { isStringNotEmpty } from "../typesAndValidationChecks";
+import {
+  characterNotLowerCharRegExp,
+  punctuationRegExp,
+  digitRegExp,
+} from "../regularExpressions";
+
 const trimStartingZeroes = (str) => {
   for (let i = 0; i < str.length; i += 1) {
     if (str[i] !== "0") {
@@ -10,4 +17,57 @@ const trimStartingZeroes = (str) => {
   return str;
 };
 
-export default trimStartingZeroes;
+const caseToUpperCase = (str) => {
+  if (!isStringNotEmpty(str)) {
+    return "";
+  }
+  const trimmedStr = str.trim();
+  let returnedStr = "";
+  let prevMatchIndex = -1;
+  let prevMatchType = "";
+
+  for (let i = 0; i < trimmedStr.length; i += 1) {
+    const char = trimmedStr[i];
+    if (punctuationRegExp.test(char)) {
+      if (
+        i !== 0 &&
+        (prevMatchIndex !== i - 1 ||
+          (prevMatchType !== "punctuation" && prevMatchIndex === i - 1))
+      ) {
+        returnedStr = `${returnedStr}_`;
+      }
+      prevMatchIndex = i;
+      prevMatchType = "punctuation";
+    } else if (digitRegExp.test(char)) {
+      if (
+        i !== 0 &&
+        (prevMatchIndex !== i - 1 ||
+          (prevMatchType !== "number" && prevMatchIndex === i - 1))
+      ) {
+        returnedStr = `${returnedStr}_${char}`;
+      } else {
+        returnedStr = `${returnedStr}${char}`;
+      }
+      prevMatchIndex = i;
+      prevMatchType = "number";
+    } else if (characterNotLowerCharRegExp.test(char)) {
+      if (
+        i !== 0 &&
+        (prevMatchIndex !== i - 1 ||
+          (prevMatchType !== "other" && prevMatchIndex === i - 1))
+      ) {
+        returnedStr = `${returnedStr}_${char}`;
+      } else {
+        returnedStr = `${returnedStr}${char}`;
+      }
+      prevMatchIndex = i;
+      prevMatchType = "other";
+    } else {
+      returnedStr = `${returnedStr}${char}`;
+    }
+  }
+
+  return returnedStr.toLocaleUpperCase();
+};
+
+export { trimStartingZeroes, caseToUpperCase };
