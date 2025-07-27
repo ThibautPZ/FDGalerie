b1f89a3ec3c4726aba2a54ce8bb989da53d592cb
ThibautPZ <thibautplez@gmail.com>
2025-07-27 19:43:10 +0200

    [BACKEND] Addition : checkPresenceInDb middleware, which provided get requests params, will reject accordingly

---
+++ backend/src/middlewares/dbCheckers/checkPresenceInDb.js
@@ -0,0 +1,138 @@
+const asyncHandler = require("express-async-handler");
+
+const CustomErrorClass = require("../../services/ErrorClasses");
+
+const {
+  isArrayNotEmpty,
+  isObjectNotEmpty,
+  isError,
+} = require("../../services/typesAndValidationChecks");
+const {
+  giveQueryPromise,
+  giveParallelQueriesPromise,
+} = require("../../helpers/dbAsyncQueriesHelper");
+
+const giveParams = (reqBody, bodyKeyParams, addedParams) => {
+  const params = {};
+  if (isObjectNotEmpty(bodyKeyParams)) {
+    for (const [param, bodyKey] of Object.entries(bodyKeyParams)) {
+      params[param] = reqBody[bodyKey];
+    }
+  }
+  if (isObjectNotEmpty(addedParams)) {
+    for (const [param, value] of Object.entries(addedParams)) {
+      params[param] = value;
+    }
+  }
+  return params;
+};
+
+const giveParamCount = (reqBody, param) => {
+  return reqBody[param].length || 0;
+};
+
+const checkPresenceInDb = (...options) => {
+  return asyncHandler(async (req, res, next) => {
+    if (!isArrayNotEmpty(options)) {
+      const err = new CustomErrorClass("00004");
+      return next(err);
+    }
+
+    if (options.length === 1) {
+      const {
+        manager,
+        method,
+        bodyKeyParams,
+        addedParams,
+        errorNumber,
+        rejectWhenTrue = false,
+        count,
+      } = options[0];
+
+      if (!manager || !method) {
+        const err = new CustomErrorClass("00004");
+        return next(err);
+      }
+
+      const params = giveParams(req.body, bodyKeyParams, addedParams);
+
+      const result = await giveQueryPromise(
+        { name: `${manager}${method}`, manager, method, queryArgs: [params] },
+        5
+      );
+
+      let doesExist = result.length > 0;
+
+      if (count) {
+        const { param, value } = count;
+        const expectedCount = value || giveParamCount(req.body, param);
+        doesExist = result === expectedCount;
+      }
+
+      if (doesExist === rejectWhenTrue) {
+        const err = new CustomErrorClass(errorNumber);
+        return next(err);
+      }
+      return next();
+    }
+
+    const errorConditions = {};
+    const queriesSpecs = options.map((option) => {
+      const {
+        manager,
+        method,
+        bodyKeyParams,
+        addedParams,
+        errorNumber,
+        rejectWhenTrue = false,
+      } = option;
+
+      if (!manager || !method) {
+        const err = new CustomErrorClass("00004");
+        return next(err);
+      }
+      const name = `${manager}${method}`;
+      Object.assign(errorConditions, {
+        [name]: { errorNumber, rejectWhenTrue },
+      });
+      const params = giveParams(req.body, bodyKeyParams, addedParams);
+
+      return {
+        name: `${manager}${method}`,
+        manager,
+        method,
+        queryArgs: [params],
+      };
+    });
+
+    const results = await giveParallelQueriesPromise(queriesSpecs, 5);
+    const errors = [];
+    for (const [name, result] of Object.entries(results)) {
+      const { errorNumber, rejectWhenTrue, count } = errorConditions[name];
+
+      if (isError(result)) {
+        errors.push(result);
+      } else {
+        let doesExist = result.length > 0;
+
+        if (count) {
+          const { param, value } = count;
+          const expectedCount = value || giveParamCount(req.body, param);
+          doesExist = result === expectedCount;
+        }
+
+        if (doesExist === rejectWhenTrue) {
+          errors.push(new CustomErrorClass(errorNumber));
+        }
+      }
+    }
+
+    if (errors.length) {
+      const err = new CustomErrorClass("01001", errors);
+      return next(err);
+    }
+    return next();
+  });
+};
+
+module.exports = checkPresenceInDb;
