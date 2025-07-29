4b18f623959604f787e012d299d5deafabf6e361
ThibautPZ <thibautplez@gmail.com>
2025-07-29 04:09:04 +0200

    [BACKEND] fix : fixed wrong body key names

--- backend/src/middlewares/reqAdders/addSupportKey.js
+++ backend/src/middlewares/reqAdders/addSupportKey.js
@@ -1,18 +1,18 @@
 const expressAsyncHandler = require("express-async-handler");
 
-const supportsFileFr = require("../../../public/locales/fr/supports.json");
-const supportsFileEnUS = require("../../../public/locales/enUS/supports.json");
-const supportsFileEnGB = require("../../../public/locales/enGB/supports.json");
+const supportFileFr = require("../../../public/locales/fr/supports.json");
+const supportFileEnUS = require("../../../public/locales/enUS/supports.json");
+const supportFileEnGB = require("../../../public/locales/enGB/supports.json");
 const giveJsonNewKeyOrError = require("../../services/giveJsonNewKeyOrError");
 
 const addSupportKey = expressAsyncHandler(async (req, res, next) => {
   const { body } = req;
-  const { supportsNameFr, supportsNameEnUS, supportsNameEnGB } = body;
+  const { supportNameFr, supportNameEnUS, supportNameEnGB } = body;
 
   const languages = {
-    fr: { file: supportsFileFr, name: supportsNameFr },
-    enUS: { file: supportsFileEnUS, name: supportsNameEnUS },
-    enGB: { file: supportsFileEnGB, name: supportsNameEnGB },
+    fr: { file: supportFileFr, name: supportNameFr },
+    enUS: { file: supportFileEnUS, name: supportNameEnUS },
+    enGB: { file: supportFileEnGB, name: supportNameEnGB },
   };
 
   const { jsonKey, error } = giveJsonNewKeyOrError(languages, "05012");
