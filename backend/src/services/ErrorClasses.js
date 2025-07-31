5f0b4d9f70a3ffa44a5a48a67baca0a8d1ffd523
ThibautPZ <thibautplez@gmail.com>
2025-07-31 05:22:19 +0200

    [BACKEND] Update : CustomErrorClass  : added content parameter to send data about current error, modified Error prototype cause parameter

--- backend/src/services/ErrorClasses.js
+++ backend/src/services/ErrorClasses.js
@@ -2,17 +2,19 @@ const errorsList = require("../../public/json/errors.json");
 const { isNotUndefined } = require("./typesAndValidationChecks");
 
 class CustomErrorClass extends Error {
-  constructor(code, errors) {
-    super(code, errors);
+  constructor(code, errors, content) {
+    super(content, { cause: errors });
     this.code = code;
     this.errors = errors || null;
+    this.content = content || null;
     this.type = this.giveType();
     this.name = this.giveName();
     this.statusCode = this.giveStatusCode();
+    this.publicCode = this.givePublicCode();
   }
 
   giveErrorInfo(field) {
-    const key = this.code;
+    const key = this.code || "00000";
     const infos = errorsList[key];
     if (!isNotUndefined(infos)) {
       return null;
@@ -42,6 +44,14 @@ class CustomErrorClass extends Error {
       return 400;
     }
     return returnedStatusCode;
+  }
+
+  givePublicCode() {
+    const returnedPublicCode = this.giveErrorInfo("publicCode");
+    if (!returnedPublicCode) {
+      return this.code;
+    }
+    return returnedPublicCode;
   }
 }
 
