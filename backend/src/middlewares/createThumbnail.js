9ff877172a5859cc2e0b4e39b790a3711511892b
ThibautPZ <thibautplez@gmail.com>
2025-08-05 20:21:27 +0200

    [BACKEND] modification : added retryability in writeThumbnail mw

--- backend/src/middlewares/createThumbnail.js
+++ backend/src/middlewares/createThumbnail.js
@@ -1,8 +1,14 @@
 const path = require("node:path");
 const fs = require("node:fs");
 const sharp = require("sharp");
+const async = require("async");
 const asyncHandler = require("express-async-handler");
+
 const CustomErrorClass = require("../services/ErrorClasses");
+const {
+  giveSuccesfulAndFailedQueryNames,
+} = require("../helpers/dbAsyncQueriesHelper");
+const { isError } = require("../services/typesAndValidationChecks");
 
 const thumbSizes = {
   medium: { pixelNum: 300, nameExtension: "md" },
@@ -41,17 +47,60 @@ const createThumbnail = (rawFileFolderName, options) => {
         fs.writeFileSync(thumbFilePath, buffer);
       } catch (error) {
         const err = new CustomErrorClass("03001", error);
-        return next(err);
+        return err;
+      }
+      return true;
+    };
+    const retryableWriteThumbnail = async.retryable(5, writeThumbnail);
+
+    const deleteThumbnail = async (size) => {
+      const { nameExtension } = thumbSizes[size];
+      const thumbFilePath = path.join(
+        __dirname,
+        `../../public/${rawFileFolderName}Thumb_${nameExtension}/${rawFileName}_${nameExtension}${thumbFileExtension}`
+      );
+      try {
+        fs.unlinkSync(thumbFilePath);
+      } catch (error) {
+        return error;
       }
       return true;
     };
 
-    if (medium) {
-      writeThumbnail("medium");
+    if (medium && !large) {
+      const result = await retryableWriteThumbnail("medium");
+      if (isError(result)) {
+        return next(result);
+      }
+      return next();
     }
 
-    if (large) {
-      writeThumbnail("large");
+    if (large && !medium) {
+      const result = await retryableWriteThumbnail("large");
+      if (isError(result)) {
+        return next(result);
+      }
+      return next();
+    }
+
+    const filesResults = await async.parallel({
+      medium: async.retryable(5, async () => writeThumbnail("medium")),
+      large: async.retryable(5, async () => writeThumbnail("large")),
+    });
+
+    const { success, failures } = giveSuccesfulAndFailedQueryNames(
+      filesResults,
+      true,
+      false
+    );
+
+    if (failures.length) {
+      const deletion = async.retryable(5, deleteThumbnail);
+      const result = await deletion(success[0]);
+      if (isError(result)) {
+        return next(new CustomErrorClass("03001", result));
+      }
+      return next(filesResults[failures[0]]);
     }
 
     return next();
