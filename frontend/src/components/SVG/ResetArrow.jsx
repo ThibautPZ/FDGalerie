c209f08e26713778c5af0cd28574da8d267770b4
ThibautPZ <thibautplez@gmail.com>
2025-08-08 04:34:14 +0200

    [FRONTEND] add : ResetArrow svg component

---
+++ frontend/src/components/SVG/ResetArrow.jsx
@@ -0,0 +1,34 @@
+export default function ResetArrow({ className }) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 25 25"
+      width={25}
+      height={25}
+      fill="#232323"
+      stroke="#232323"
+      strokeWidth="1.8690000000000002"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <g id="SVGRepo_iconCarrier">
+        <g
+          fill="none"
+          fillRule="evenodd"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          transform="translate(2 2)"
+        >
+          <path d="m12.5 1.5c2.4138473 1.37729434 4 4.02194088 4 7 0 4.418278-3.581722 8-8 8s-8-3.581722-8-8 3.581722-8 8-8" />
+          <path d="m12.5 5.5v-4h4" />
+        </g>
+      </g>
+    </svg>
+  );
+}
