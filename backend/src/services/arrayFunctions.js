3fe4d55245a0941146c80f8c1bd6dd867e3e01df
ThibautPZ <thibautplez@gmail.com>
2025-08-15 05:41:26 +0200

    [BACKEND] modification : added giveValuesExclusivesBetween2Arrays function

--- backend/src/services/arrayFunctions.js
+++ backend/src/services/arrayFunctions.js
@@ -8,4 +8,17 @@ const giveErrorInstances = (array) => {
   return returnedArr;
 };
 
-module.exports = { giveErrorInstances };
+const giveValuesExclusivesBetween2Arrays = (arr1, arr2) => {
+  if (!isArrayNotEmpty(arr1) || !isArrayNotEmpty(arr2)) {
+    return [[], []];
+  }
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const exclusivesToArr1 = arr1.filter((item) => !set2.has(item));
+  const exclusivesToArr2 = arr2.filter((item) => !set1.has(item));
+
+  return [exclusivesToArr1, exclusivesToArr2];
+};
+
+module.exports = { giveErrorInstances, giveValuesExclusivesBetween2Arrays };
