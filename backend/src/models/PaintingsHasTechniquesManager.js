6d850d64aac43fcc0b5a79c65271054c803d93a8
ThibautPZ <thibautplez@gmail.com>
2025-08-23 03:40:09 +0200

    [BACKEND] modified : added findPaintingsIdByTechniqueId method

--- backend/src/models/PaintingsHasTechniquesManager.js
+++ backend/src/models/PaintingsHasTechniquesManager.js
@@ -9,6 +9,36 @@ class PaintingsHasTechniquesManager extends AbstractManager {
     return this.database.query(`SELECT name, id FROM ${this.table} `);
   }
 
+  async findPaintingsIdByTechniqueId({ techniqueId }) {
+    return this.database.query(
+      `SELECT pht.paintings_id AS id
+      FROM ${this.table} AS pht
+      WHERE pht.techniques_id = ?`,
+      [techniqueId]
+    );
+  }
+
+  async findAllPaintingsByTechniqueId(techniqueId) {
+    return this.database.query(
+      `SELECT p.id AS id, p.title AS title, p.width AS width, p.height AS height, p.paintings_availabilities_id AS oeuvreAvailability, p.publicly_visible AS publiclyVisible,
+      pa.name AS availabilityName, p.family_member AS familyMember, p.families_id AS familyId, JSON_ARRAYAGG(t.name) AS techniques,
+      f.name AS family, ps.name AS format, s.name as support,
+      pst.file_name AS fileName, pst.file_extension AS fileExtension, pac.fr_comment AS artistCommentFr, pac.en_US_comment AS artistCommentEnUS, pac.en_GB_comment AS artistCommentEnGB
+      FROM ${this.table} AS pht
+      LEFT JOIN paintings AS p ON pht.paintings_id = p.id
+      LEFT JOIN painting_sizes AS ps ON p.painting_sizes_id = ps.id
+      LEFT JOIN families AS f ON p.families_id = f.id
+      LEFT JOIN supports AS s ON p.supports_id = s.id
+      LEFT JOIN paintings_artist_comments AS pac ON p.id = pac.paintings_id
+      LEFT JOIN paintings_availabilities AS pa ON p.paintings_availabilities_id = pa.id
+      LEFT JOIN techniques AS t ON pht.techniques_id = t.id
+      LEFT JOIN paintings_storages AS pst ON p.id = pst.paintings_id
+      GROUP BY p.id
+      HAVING SUM(pht.techniques_id = ?) > 0;`,
+      [techniqueId]
+    );
+  }
+
   async createPaintingTechniqueRelationsByPaintingId(paintingId, techniques) {
     const values = [];
     const queryTail = techniques
@@ -31,27 +61,6 @@ class PaintingsHasTechniquesManager extends AbstractManager {
     const queryValues = [paintingId, ...techniques];
 
     return this.database.query(queryBody, queryValues);
-  }
-
-  async findAllPaintingsByTechniqueId(techniqueId) {
-    return this.database.query(
-      `SELECT p.id AS id, p.title AS title, p.width AS width, p.height AS height, p.paintings_availabilities_id AS oeuvreAvailability, p.publicly_visible AS publiclyVisible,
-      pa.name AS availabilityName, p.family_member AS familyMember, p.families_id AS familyId, JSON_ARRAYAGG(t.name) AS techniques,
-      f.name AS family, ps.name AS format, s.name as support,
-      pst.file_name AS fileName, pst.file_extension AS fileExtension, pac.fr_comment AS artistCommentFr, pac.en_US_comment AS artistCommentEnUS, pac.en_GB_comment AS artistCommentEnGB
-      FROM ${this.table} AS pht
-      LEFT JOIN paintings AS p ON pht.paintings_id = p.id
-      LEFT JOIN painting_sizes AS ps ON p.painting_sizes_id = ps.id
-      LEFT JOIN families AS f ON p.families_id = f.id
-      LEFT JOIN supports AS s ON p.supports_id = s.id
-      LEFT JOIN paintings_artist_comments AS pac ON p.id = pac.paintings_id
-      LEFT JOIN paintings_availabilities AS pa ON p.paintings_availabilities_id = pa.id
-      LEFT JOIN techniques AS t ON pht.techniques_id = t.id
-      LEFT JOIN paintings_storages AS pst ON p.id = pst.paintings_id
-      GROUP BY p.id
-      HAVING SUM(pht.techniques_id = ?) > 0;`,
-      [techniqueId]
-    );
   }
 }
 
