042a23b9f25e593b20e436e28474c2d729209f6b
ThibautPZ <thibautplez@gmail.com>
2025-08-27 03:28:55 +0200

    [BACKEND] fix : false rejection when modifying a painting without any file in query

--- backend/src/middlewares/reqCheckers/checkPaintingVisibility.js
+++ backend/src/middlewares/reqCheckers/checkPaintingVisibility.js
@@ -1,12 +1,24 @@
 const expressAsyncHandler = require("express-async-handler");
 const CustomErrorClass = require("../../services/ErrorClasses");
+const { isObjectNotEmpty } = require("../../services/typesAndValidationChecks");
 
 const checkPaintingVisibility = expressAsyncHandler(async (req, res, next) => {
   const { body, query } = req;
 
-  if (body.oeuvreVisibility === true && query.fileFields === "none") {
+  const { oeuvreFileDeleteFile, detailedPaintingData } = body;
+
+  if (body.oeuvreVisibility === false) {
+    return next();
+  }
+
+  if (!isObjectNotEmpty(detailedPaintingData) && query.fileFields === "none") {
     return next(new CustomErrorClass("05004"));
   }
+
+  if (oeuvreFileDeleteFile && query.fileFields === "none") {
+    return next(new CustomErrorClass("05004"));
+  }
+
   return next();
 });
 module.exports = checkPaintingVisibility;
